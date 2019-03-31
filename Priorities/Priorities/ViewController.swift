61765234cec055847ac37758c50e80219377713b
Quốc Khải <quockhai.vn@gmail.com>
2019-03-31 22:38:59 +0700

    Priorities

---
+++ Priorities/Priorities/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Priorities
+//
+//  Created by Quốc Khải on 3/31/19.
+//  Copyright © 2019 Quốc Khải. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.removeFromSuperview()
+    }
+    
+}
+
