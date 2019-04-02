3d6a99ea891a7d8c0d61cf6392b83bfb29d33b02
Quốc Khải <quockhai.vn@gmail.com>
2019-04-02 17:03:44 +0700

    Sync

---
+++ CameraLayout/CameraLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraLayout
+//
+//  Created by quockhai on 2019/4/1.
+//  Copyright © 2019 Polymath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Constraints/Constraints/KQCameraView.swift
@@ -0,0 +1,21 @@
+//
+//  KQCameraView.swift
+//  Constraints
+//
+//  Created by quockhai on 2019/4/2.
+//  Copyright © 2019 Quốc Khải. All rights reserved.
+//
+
+import UIKit
+
+class KQCameraView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
